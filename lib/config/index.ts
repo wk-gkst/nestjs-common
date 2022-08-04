c1939c3949b4e86a332069c28f3bf58fd38a6133
williamkhoo <william_khoo@outlook.com>
2022-08-04 10:58:43 +0800

    implement config validation

--- lib/config/index.ts
+++ lib/config/index.ts
@@ -2,5 +2,13 @@ import DbConfig from "./db.config";
 import MongoConfig from "./mongodb.config";
 import RedisConfig from "./redis.config";
 import RabbitMQConfig from "./rabbitmq.config";
+import JoiUtil, { JoiConfig } from "./joi-util";
 
-export { DbConfig, MongoConfig, RedisConfig, RabbitMQConfig };
+export {
+  DbConfig,
+  MongoConfig,
+  RedisConfig,
+  RabbitMQConfig,
+  JoiUtil,
+  JoiConfig,
+};
